0313e46735997ca4f20f1f9640ecf6e7711ff818
Felipe Soares <felipe_soares_2@hotmail.com>
2019-06-18 01:38:58 -0300

    edit

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -2,12 +2,9 @@ import React, {
     Component
 } from 'react';
 import api from '../../services/api';
-import sha256 from 'crypto-js/sha256';
 
 import './styles.css';
-// import { Container } from './styles';
 
-const CryptoJS = require('crypto-js');
 
 export default class User extends Component {
     state = {
